e33e48a3345fcbd6fdf1d2edee85c7a626f5c7e3
Julian Cardone <juliancardone1@gmail.com>
2023-02-24 11:36:29 -0500

    add the moving funcitonality" git push

--- board.py
+++ board.py
@@ -6,21 +6,31 @@ class Board:
         self.sentinel = object()
         self.make_starting_grid()
 
+    def move_piece(self, start_pos, end_pos):
+
+        #add functionality for rasiing errors when the end pos is invalid
+        if not self.valid_pos(start_pos):
+            raise NameError("The start position is invalid!")
+        if not self.valid_pos(end_pos):
+            raise NameError("The end position is invalid!")
+        
+        #if there is no piece at the start pos
+        if self.rows[start_pos[1]][start_pos[0]] == None:
+            raise NameError("This spot is empty!")
+
+        #if there are no errors then move the piece!
+        self.rows[end_pos[1]][end_pos[0]] = self.rows[start_pos[1]][start_pos[0]]
+        self.rows[start_pos[1]][start_pos[0]] = None
+
     def make_starting_grid(self):
         self.rows = [[self.sentinel]*8]*8
         #populate white and black pieces
 
-    def move_piece(self, start_pos, end_pos):
-        #if there is no piece at the start pos
-        if self.board[start_pos[1]][start_pos[0]] == None:
-            raise NameError("This spot is empty!")
-        #add functionality for rasiing errors when the end pos is invalid
-        if (end_pos[0] < 1 or end_pos[0] > 8) or (end_pos[1] < 1 or end_pos[0] > 8):
-            raise NameError("The end position is invalid!")
-        
-        #if there are no errors then move the piece!
-        self.board[end_pos[1]][end_pos[0]] = self.board[start_pos[1]][start_pos[0]]
-        self.board[start_pos[1]][start_pos[0]] = None
+    def valid_pos(self, pos):
+        return all((x >= 0 and x <= 7) for x in pos)
+
+    
 
 b = Board()
-print(b.rows)
+# print(b.rows)
+b.move_piece((0,0),(1,1))
