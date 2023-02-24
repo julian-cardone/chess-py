874607ba174b3a9502a98f7c9ca8377f0c8e8b1c
Julian Cardone <juliancardone1@gmail.com>
2023-02-24 11:51:39 -0500

    add check check

--- board.py
+++ board.py
@@ -6,7 +6,19 @@ class Board:
         self.sentinel = object()
         self.make_starting_grid()
 
-    def move_piece(self, start_pos, end_pos):
+    def add_piece(self, piece, pos):
+        
+
+    def move_piece(self, turn_color, start_pos, end_pos):
+
+        piece = self.rows[start_pos[1]][start_pos[0]]
+
+        if piece.color != turn_color
+            raise NameError("You must move your own piece")
+        elif end_pos not in piece.moves
+            raise NameError("Piece does not move like that")
+        elif end_pos not in piece.valid_moves
+            raise NameError("You cannot move into check!")
 
         #add functionality for rasiing errors when the end pos is invalid
         if not self.valid_pos(start_pos):
@@ -15,12 +27,14 @@ class Board:
             raise NameError("The end position is invalid!")
         
         #if there is no piece at the start pos
+        #change this None to self.sentinel
         if self.rows[start_pos[1]][start_pos[0]] == None:
             raise NameError("This spot is empty!")
 
         #if there are no errors then move the piece!
-        self.rows[end_pos[1]][end_pos[0]] = self.rows[start_pos[1]][start_pos[0]]
-        self.rows[start_pos[1]][start_pos[0]] = None
+        self.rows[end_pos[1]][end_pos[0]] = piece
+        self.rows[start_pos[1]][start_pos[0]] = self.sentinel 
+        piece.pos = end_pos
 
     def make_starting_grid(self):
         self.rows = [[self.sentinel]*8]*8
